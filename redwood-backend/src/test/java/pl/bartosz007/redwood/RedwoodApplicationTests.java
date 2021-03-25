51aa0703ec486432b4341c03b0c041912dedafc0
Bartosz007 <bartoszkra98@gmail.com>
2021-03-25 14:25:13 +0100

    creating project structure and static web pages

---
+++ redwood-backend/src/test/java/pl/bartosz007/redwood/RedwoodApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.bartosz007.redwood;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedwoodApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
